1d9216355c7947ef9198c9a63c8df149ca7e4736
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-07-25 19:10:42 -0400

    Update sh_config.lua

--- lua/darkrp-ls/sh_config.lua
+++ lua/darkrp-ls/sh_config.lua
@@ -11,6 +11,24 @@
 -- English: "Level"
 -- Spanish: "Nivel"
 
+
+----------------------------------
+--------- Generic Config ---------
+----------------------------------
 darkrp_ls.prefix = "[DarkRP-LS]" -- The prefix on the chat.
 darkrp_ls.prefix_color = Color(0, 255, 0) -- The color of the prefix.
 darkrp_ls.level_name = "Level" -- The name of the level.
+
+
+----------------------------------
+--------- Donator Config ---------
+----------------------------------
+darkrp_ls.vip_group = {
+    "vip",
+    "donator",
+    "supporter",
+    "moderator",
+    "admin",
+    "superadmin",
+}   -- The groups that are VIP
+darkrp_ls.vip_multiplier = 1.5 -- The multiplier for VIPs.
