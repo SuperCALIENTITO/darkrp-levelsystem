04e0f85d5767886fc27db6512b4d874f73c0b09f
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-07-26 15:51:57 -0400

    Update sh_core.lua

--- lua/darkrp-ls/shared/sh_core.lua
+++ lua/darkrp-ls/shared/sh_core.lua
@@ -2,6 +2,8 @@
 ------------ Functions -----------
 ----------------------------------
 function DLS_getLevelPlayer(ply)
+    if not IsValid(ply) then return false end
+
     local data = sql.Query("SELECT level FROM " .. darkrp_ls.db .. " WHERE player = " .. ply:SteamID64() .. ";")
     if #darkrp_ls["levels"] > tonumber(data[1].level) then
         return tonumber(data[1].level)
@@ -11,23 +13,39 @@ function DLS_getLevelPlayer(ply)
 end
 
 function DLS_getXPPlayer(ply)
+    if not IsValid(ply) then return false end
+
     local data = sql.Query("SELECT xp FROM " .. darkrp_ls.db .. " WHERE player = " .. ply:SteamID64() .. ";")
     return tonumber(data[1].xp)
 end
 
 function DLS_getLevelExp(level)
+    if not level or isnumber(level) then return false end
+
     local xp = darkrp_ls["levels"][level]
     return tonumber(xp)
 end
 
-function DLS_checkPlayerDatabase(ply)
-    local data = sql.Query("SELECT * FROM " .. darkrp_ls.db .. " WHERE player = " .. ply:SteamID64() .. ";")
-    if not data then
-        sql.Query("INSERT INTO " .. darkrp_ls.db .. " (player, plyname) VALUES (" .. ply:SteamID64() .. ", " .. sql.SQLStr(ply:Name()) .. ");")
-    end
+function DLS_setLevelPlayer(ply, level)
+    if not IsValid(ply) then return false end
+    if not isnumber(level) then return false end
+
+    sql.Query("UPDATE " .. darkrp_ls.db .. " SET level = " .. level .. " WHERE player = " .. ply:SteamID64() .. ";")
+    return true
+end
+
+function DLS_setXPPlayer(ply, xp)
+    if not IsValid(ply) then return false end
+    if not isnumber(xp) then return false end
+
+    sql.Query("UPDATE " .. darkrp_ls.db .. " SET xp = " .. xp .. " WHERE player = " .. ply:SteamID64() .. ";")
+    return true
 end
 
 function DLS_addXPToPlayer(ply, xp)
+    if not IsValid(ply) then return false end
+    if not isnumber(xp) then return false end
+
     if table.HasValue(darkrp_ls.vip_group, ply:GetUserGroup()) then
         xp = math.Round(xp * darkrp_ls.vip_multiplier)
     end
@@ -47,10 +65,26 @@ function DLS_addXPToPlayer(ply, xp)
     else
         sql.Query("UPDATE " .. darkrp_ls.db .. " SET xp = " .. xp .. " WHERE player = " .. ply:SteamID64() .. ";")
     end
+
+    return true
 end
 
 function DLS_updatePlayerName(ply)
+    if not IsValid(ply) then return false end
+
     sql.Query("UPDATE " .. darkrp_ls.db .. " SET plyname = " .. sql.SQLStr(ply:Name()) .. " WHERE player = " .. ply:SteamID64() .. ";")
+    return true
+end
+
+function DLS_checkPlayerDatabase(ply)
+    if not IsValid(ply) then return false end
+
+    local data = sql.Query("SELECT * FROM " .. darkrp_ls.db .. " WHERE player = " .. ply:SteamID64() .. ";")
+    if not data then
+        sql.Query("INSERT INTO " .. darkrp_ls.db .. " (player, plyname) VALUES (" .. ply:SteamID64() .. ", " .. sql.SQLStr(ply:Name()) .. ");")
+    end
+
+    return true
 end
 
 ----------------------------------
