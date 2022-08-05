520c1598fb5d6112578cbe2ee62a9f7079ec0d2e
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-08-05 15:19:33 -0400

    Create sh_commands.lua

---
+++ lua/darkrp-ls/shared/sh_commands.lua
@@ -0,0 +1,31 @@
+local function setPlayerLevel(ply, cmd, args)
+    local target = args[1]
+
+    for _, v in pairs(player.GetAll()) do
+        if string.find(string.lower(v:Nick()), target) then
+            v:SetPlayerLevel(args[2])
+            MsgC(darkrp_ls.prefix_color, darkrp_ls.prefix .. "Set " .. v:Nick() .. "'s level to " .. args[2] .. ".")
+        end
+    end
+
+end
+
+local function autoComplete( cmd, args )
+
+    args = string.Trim( args )
+
+    local tbl = {}
+
+    for _, v in pairs( player.GetAll() ) do
+        local name = v:Nick()
+        if string.find( string.lower( name ), args ) then
+            nick = "\"" .. name .. "\""
+            nick = "level_setlevel " .. nick
+            table.insert( tbl, nick )
+        end
+    end
+
+    return tbl
+end
+
+concommand.Add("level_setlevel", setPlayerLevel, autoComplete)
